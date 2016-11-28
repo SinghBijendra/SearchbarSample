2b42826dd5547105fdd1039bc2ab3df84dbc50e8
SinghBijendra <rebijendra@gmail.com>
2016-11-28 11:07:04 +0530

    Initial commit

---
+++ app/src/main/java/com/bijendra/search/MainActivity.java
@@ -0,0 +1,68 @@
+package com.bijendra.search;
+
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+   @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mf=getMenuInflater();
+        mf.inflate(R.menu.options_menu,menu);
+       MenuItem searchItem = menu.findItem(R.id.search);
+       SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+       searchView.setOnQueryTextListener(this);
+
+       SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+       searchView.setSearchableInfo(searchManager.getSearchableInfo(
+               new ComponentName(this, MainActivity.class)));
+       searchView.setIconifiedByDefault(false);
+
+
+       return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(this, "Searching by: "+ query, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+}
